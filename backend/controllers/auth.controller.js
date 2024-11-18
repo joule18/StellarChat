2a1c11686e73f8347f100b7683a9b273d1d00969
joule18 <jrjundam18@gmail.com>
2024-11-18 13:01:52 +0800

    Added logout endpoint

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -88,7 +88,13 @@ const login = async (req, res) => {
   }
 };
 const logout = (req, res) => {
-  res.send("logoutUser");
+  try {
+    res.cookie("jwt", "", { maxAge: 0 });
+    res.status(200).json({ message: "Logged out successfully." });
+  } catch (error) {
+    console.log("Error in logout controller ", error.message);
+    res.status(500).json({ error: "Internal server error." });
+  }
 };
 
 export { login, logout, signup };
