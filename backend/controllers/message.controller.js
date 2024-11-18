a1b3b51ee87f6e820e59cd27b3058d449a0d8a7f
joule18 <jrjundam18@gmail.com>
2024-11-18 14:50:20 +0800

    Added getMessages endpoint; edited sendMessage controller

--- backend/controllers/message.controller.js
+++ backend/controllers/message.controller.js
@@ -8,13 +8,19 @@ const sendMessage = async (req, res) => {
     const { id: receiverId } = req.params;
     const senderId = req.user._id;
 
+    const receiverConfirm = await User.findById(receiverId);
+    if (!receiverConfirm) {
+      console.log("Error: Receiver user not found. (Send Message)");
+      return res.status(404).json({ error: "User not found." });
+    }
+
     let conversation = await Conversation.findOne({
       participants: { $all: [senderId, receiverId] },
     });
 
     if (!conversation) {
       //no conversation => sending a message for the first time
-      conversation = Conversation.create({
+      conversation = await Conversation.create({
         participants: [senderId, receiverId],
       });
     }
@@ -27,20 +33,43 @@ const sendMessage = async (req, res) => {
 
     if (newMessage) {
       conversation.messages.push(newMessage._id);
-
-      //ADD SOCKET IO FUNCTIONALITY
-
-      await Promise.all([conversation.save(), newMessage.save()]);
-
-      res.status(201).json(newMessage);
-    } else {
-      console.log("Error in creating new message: ", error.message);
-      res.status(400).json({ error: "Error in sending message." });
     }
+
+    //ADD SOCKET IO FUNCTIONALITY
+
+    await Promise.all([conversation.save(), newMessage.save()]);
+    res.status(201).json(newMessage);
   } catch (error) {
     console.log("Error in sendMessage controller: ", error.message);
     res.status(500).json({ error: "Internal server error." });
   }
 };
 
-export { sendMessage };
+const getMessages = async (req, res) => {
+  try {
+    const { id: userToChatId } = req.params;
+    const senderId = req.user._id;
+
+    const receiverConfirm = await User.findById(userToChatId);
+    if (!receiverConfirm) {
+      console.log("Error: Receiver user not found. (Get Messages)");
+      return res.status(404).json({ error: "User not found." });
+    }
+
+    const conversation = await Conversation.findOne({
+      participants: { $all: [senderId, userToChatId] },
+    }).populate("messages");
+
+    if (!conversation) {
+      return res.status(200).json([]);
+    }
+
+    const messages = conversation.messages;
+    res.status(200).json(messages);
+  } catch (error) {
+    console.log("Error in getMessages controller: ", error.message);
+    res.status(500).json({ error: "Internal server error." });
+  }
+};
+
+export { sendMessage, getMessages };
